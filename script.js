3b7be6b327f48c7a62c2264a69a842ec4e97e2ca
codemastermahmudullah <codemastermahmudullah@gmail.com>
2024-11-29 03:59:41 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Frontend Developer", "YouTuber", " Blogger"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    BackDelay: 1000,
+    loop: true
+})
+
+var typed = new Typed(".multiple-text2", {
+    strings: ["PORTFOLIO", "MAHMUDULLAH"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    BackDelay: 1000,
+    loop: true
+})
